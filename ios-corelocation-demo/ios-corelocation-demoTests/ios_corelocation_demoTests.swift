1305640d08999d5ccea10a9ef348c1709f6a76b0
y.okudera <mail@sample.com>
2017-02-25 20:04:16 +0900

    Add SampleCode

---
+++ ios-corelocation-demo/ios-corelocation-demoTests/ios_corelocation_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_corelocation_demoTests.swift
+//  ios-corelocation-demoTests
+//
+//  Created by OkuderaYuki on 2017/02/23.
+//  Copyright © 2017年 YukiOkudera. All rights reserved.
+//
+
+import XCTest
+@testable import ios_corelocation_demo
+
+class ios_corelocation_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
